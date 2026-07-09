afb661889af12e9563d2a6218ba407f587345961
Cameron Hales <cameron.hales@ons.gov.uk>
2026-07-09 17:22:51 +0100

    feat: WIP fleshout wireframe for other submodules

--- src/main.R
+++ src/main.R
@@ -1,1 +1,21 @@
+# ---
 # file will pull in all parts of the modelling pipeline
+source("src/data_processing_2.R")
+source("src/quality_assurance/output_summary.R")
+
+# ---
+# section 1
+    # previous data processing steps go here, raser mosaicking ect.
+
+    # call in data procssing
+    output_df <- data_processing_wrapper()
+
+    # call in qa function that reads output of data processing
+    # qa_checks and outputs
+    
+    # user inputs prompt questions
+    ## does checks look good?
+    ## 
+# ---
+
+    ## Further pipeline
---
+++ src/quality_assurance/output_summary.R
@@ -0,0 +1,4 @@
+# this will house our quality checks
+
+## look into library 'modelsummary' as a quick fix
+## https://modelsummary.com/vignettes/datasummary.html#datasummary_skim
