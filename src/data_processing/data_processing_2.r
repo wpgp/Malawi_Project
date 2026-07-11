d89f5b5b9839b0b8a8121c949771088166c375a6
Cameron Hales <cameron.hales@ons.gov.uk>
2026-07-11 12:18:00 +0100

    :construction:feat: WIP integrate census processing data into data processing

--- src/data_processing/data_processing_2.r
+++ src/data_processing/data_processing_2.r
@@ -5,9 +5,11 @@
 library(nngeo)
 library(haven)
 library(tidyverse)
+library(logger)
 
 # functions from within this package
 source("utils.R")
+source("src/data_processing/data_processing_helpers.R")
 
 # load all variables into config
 # TODO: little bit worried about how much less visible this is!
@@ -18,7 +20,9 @@ if (!is.null(config)) {
 }
 
 
+log_info("Begining data Processing,")
 
+log_info("dp1 - Loading config, reading data..")
 #Specify Drive Path
 data_dirs <- config$paths
 
@@ -30,14 +34,19 @@ shapefile_path <- file.path(drive_path, data_dirs$shapefile_dir)
 data_sources <- config$sources
 
 #Load datasets
-mphc_2018 <- read_dta(file.path(input_path, data_sources$mphc))
-ICT_data <- read_dta(file.path(input_path, data_sources$ict))
-IHS6_data <- read_dta(file.path(input_path, data_sources$ihs6))
-Naca_data <- read_dta(file.path(input_path, data_sources$naca))
-dhs_data <- read_dta(file.path(input_path, data_sources$dhs_survey))
-dhs_listing <- read_dta(file.path(input_path, data_sources$dhs_list))
+mphc_2018 <- read_dta(file.path(input_path, data_sources$mphc$data_file))
+ICT_data <- read_dta(file.path(input_path, data_sources$ict$data_file))
+IHS6_data <- read_dta(file.path(input_path, data_sources$ihs6$data_file))
+Naca_data <- read_dta(file.path(input_path, data_sources$naca$data_file))
+dhs_data <- read_dta(file.path(input_path, data_sources$dhs_survey$data_file))
+dhs_listing <- read_dta(file.path(input_path, data_sources$dhs_list$data_file))
 
 ea <- st_read(file.path(shapefile_path, "2018_MPHC_EAs_Final_for_Use.shp")) # replaces "2018_MPHC_EAs_Final_for_Use_Corrected.shp"
+log_info("dp1 - .. config and datasets loaded successfully")
+
+
+log_info("dp2 - cleaning census data")
+mphc_pop_no_gps <- clean_census_data(mphc_2018)
 
 #' This function will process all steps in the data_processing_2 pipeline
 #' 
