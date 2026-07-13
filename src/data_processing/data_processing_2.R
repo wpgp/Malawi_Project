0c5187de90eaf6ac1f088fab15fddd14f1e42177
Amanda Baizan Edge <amanda.baizan.edge@ons.gov.uk>
2026-07-13 15:08:30 +0100

    Fixed output path typo. I am able to reproduce up until "mphc_rbind" step (line 334 in 00_Data_Processing2.R")

--- src/data_processing/data_processing_2.R
+++ src/data_processing/data_processing_2.R
@@ -12,30 +12,25 @@ source("utils.R")
 source("src/data_processing/data_processing_helpers.R")
 
 #' This function will process all steps in the data_processing_2 pipeline
-#' 
-#' 
+#'
+#'
 data_processing_wrapper <- function() {
 
   log_info("Begining data Processing,")
 
   # setup
-
   ## this section includes loading config, variables and any datasets
-
   config <- get("load_config", mode = "function")()
-
   if (is.null(config)) {
-      stop("Config could not be loaded.")
+    stop("Config could not be loaded.")
   }
-
   print("Config loaded")
-
   log_info("dp1 - Loading config, reading data..")
 
   data_dirs <- config$paths
   drive_path <- data_dirs$drive_path
   input_path <- file.path(drive_path, data_dirs$mnso_data_dir)
-  output_path <- file.path(drive_path, data_dirs$output_dirs)
+  output_path <- file.path(drive_path, data_dirs$output_dir)
   shapefile_path <- file.path(drive_path, data_dirs$shapefile_dir)
 
   data_sources <- config$sources
@@ -44,9 +39,7 @@ data_processing_wrapper <- function() {
   ea <- sf::st_read(file.path(shapefile_path, "2018_MPHC_EAs_Final_for_Use.shp"))
   log_info("dp1 - .. config and datasets loaded successfully")
 
-
   # data processing
-
   ## this section calls in functions from the  helper script to clean
   ## and process the data
 
