99c671622524ea67a7ebb167bbde9f1d56287319
Amanda Baizan Edge <amanda.baizan.edge@ons.gov.uk>
2026-07-13 17:12:18 +0100

    Added a generic  function to handle both ICT and IHS6 data which is handled by config

--- src/data_processing/data_processing_2.R
+++ src/data_processing/data_processing_2.R
@@ -34,8 +34,11 @@ data_processing_wrapper <- function() {
   shapefile_path <- file.path(drive_path, data_dirs$shapefile_dir)
 
   data_sources <- config$sources
+  data_thresholds <- config$thresholds
 
   mphc_2018 <- read_dta(file.path(input_path, data_sources$mphc$data_file))
+  ICT_data <- read_dta(file.path(input_path, data_sources$ict$data_file))
+  IHS6_data <- read_dta(file.path(input_path, data_sources$ihs6$data_file))
   ea <- sf::st_read(file.path(shapefile_path, "2018_MPHC_EAs_Final_for_Use.shp"))
   log_info("dp1 - .. config and datasets loaded successfully")
 
@@ -46,9 +49,33 @@ data_processing_wrapper <- function() {
   log_info("dp2 - processing census data")
   mphc_rbind <- get("process_census_data", mode = "function")(mphc_2018, ea, output_path)
 
+  log_info("dp3 - processing ICT data")
+  ICT_rbind <- get("process_gps_household_data", mode = "function")(
+    survey_data = ICT_data,
+    ea_shapefile = ea,
+    source_config = data_sources$ict,
+    output_count_col = "ict_hh_count",
+    gps_accuracy_threshold_m = data_thresholds$gps_accuracy_threshold_m
+  )
+
+  output_df <- mphc_rbind %>%
+    left_join(ICT_rbind, by = c("EA_CODE" = "EA_Number"))
+
+  log_info("dp4 - processing IHS6 data")
+  IHS_rbind <- get("process_gps_household_data", mode = "function")(
+    survey_data = IHS6_data,
+    ea_shapefile = ea,
+    source_config = data_sources$ihs6,
+    output_count_col = "ihs_hh_count",
+    gps_accuracy_threshold_m = data_thresholds$gps_accuracy_threshold_m
+  )
+
+  output_df <- output_df %>%
+    left_join(IHS_rbind, by = "EA_CODE")
+
   # output section
 
   ## write out outputs here
 
-  return(mphc_rbind)
+  return(output_df)
 }
--- src/data_processing/data_processing_helpers.R
+++ src/data_processing/data_processing_helpers.R
@@ -242,3 +242,70 @@ process_census_data <- function(mphc_data, ea_shapefile, output_path) {
 
     return(mphc_rbind)
 }
+
+
+#' Generic processor for GPS-based household listing data at EA level.
+#'
+#' @param survey_data (tibble) source listing data.
+#' @param ea_shapefile (sf) EA boundaries used for nearest-neighbour assignment.
+#' @param source_config (list) source-specific config containing EA, accuracy,
+#'      longitude and latitude column names.
+#' @param output_count_col (character) name of the output count column.
+#' @param gps_accuracy_threshold_m (numeric) threshold in meters used to split
+#'      original EA assignment versus spatial reassignment.
+#'
+#' @return survey_rbind (tibble) EA-level household counts.
+process_gps_household_data <- function(
+    survey_data,
+    ea_shapefile,
+    source_config,
+    output_count_col,
+    gps_accuracy_threshold_m
+) {
+
+    source_ea_col <- source_config$source_ea_col
+    accuracy_col <- source_config$accuracy_col
+    longitude_col <- source_config$longitude_col
+    latitude_col <- source_config$latitude_col
+    reassigned_ea_col <- paste0(source_ea_col, "_spatial")
+
+    survey_data <- survey_data %>%
+        mutate(hh_count = 1)
+
+    no_gps <- survey_data %>%
+        filter(is.na(.data[[longitude_col]]) | is.na(.data[[latitude_col]])) %>%
+        group_by(.data[[source_ea_col]]) %>%
+        summarise(hh_count = sum(hh_count, na.rm = T), .groups = "drop") %>%
+        rename(!!output_count_col := hh_count)
+
+    survey_sf <- survey_data %>%
+        drop_na(all_of(c(longitude_col, latitude_col))) %>%
+        st_as_sf(coords = c(longitude_col, latitude_col))
+
+    st_crs(survey_sf) <- 4326
+    survey_sf <- st_transform(survey_sf, crs = st_crs(ea_shapefile))
+
+    nearest_indices <- st_nearest_feature(survey_sf, ea_shapefile)
+    survey_sf[[reassigned_ea_col]] <- ea_shapefile$EA_CODE[nearest_indices]
+
+    survey_sf <- survey_sf %>% as_tibble()
+
+    greater <- survey_sf %>%
+        filter(.data[[accuracy_col]] > gps_accuracy_threshold_m) %>%
+        group_by(.data[[source_ea_col]]) %>%
+        summarise(hh_count = sum(hh_count, na.rm = T), .groups = "drop") %>%
+        rename(!!output_count_col := hh_count)
+
+    less <- survey_sf %>%
+        filter(.data[[accuracy_col]] < gps_accuracy_threshold_m) %>%
+        group_by(.data[[reassigned_ea_col]]) %>%
+        summarise(hh_count = sum(hh_count, na.rm = T), .groups = "drop") %>%
+        rename(!!source_ea_col := .data[[reassigned_ea_col]]) %>%
+        rename(!!output_count_col := hh_count)
+
+    survey_rbind <- bind_rows(no_gps, greater, less) %>%
+        group_by(.data[[source_ea_col]]) %>%
+        summarise(across(everything(), \(x) sum(x, na.rm = TRUE)), .groups = "drop")
+
+    return(survey_rbind)
+}
